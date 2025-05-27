be9e550c38ea88f044deb90fed4180e036431b65
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-27 17:53:49 -0300

    feat: add clinic creation redirection logic to dashboard based on user associations

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,7 @@
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -9,6 +12,14 @@ const DashboardPage = async () => {
 
   if (!session?.user) {
     redirect("/authentication");
+  }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/create-clinic");
   }
 
   return <div>Dashboard</div>;
